5f474d0aa09ec1a6cfce444585e2ddc7e2fe30d8
reheat <reheat1540@gmail.com>
2023-10-06 18:07:48 +0900

    Initial commit: 기본 프로젝트 설정 완료

---
+++ src/main/java/com/reheat/reheatlog/reheatLogApplication.java
@@ -0,0 +1,13 @@
+package com.reheat.reheatlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class reheatLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(reheatLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reheat/reheatlog/reheatLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reheat.reheatlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class reheatLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
