72ed9285ffb3da8618fbc8a1307297fb6d5d141d
reheat <reheat1540@gmail.com>
2023-10-06 20:40:32 +0900

    chore: java 파일 이름 변경 (JAVA 파일은 첫 글자가 대문자, 패키지 이름은 소문자로 하기로 함)

--- src/main/java/com/reheat/reheatlog/reheatLogApplication.java
+++ src/main/java/com/reheat/reheatlog/reheatLogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class reheatLogApplication {
+public class ReheatLogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(reheatLogApplication.class, args);
+		SpringApplication.run(ReheatLogApplication.class, args);
 	}
 
 }
