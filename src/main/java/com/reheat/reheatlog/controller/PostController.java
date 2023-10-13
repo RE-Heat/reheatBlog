53d166d99fa061b8774feccbdb8cb73f70a73be5
reheat <reheat1540@gmail.com>
2023-10-13 19:06:05 +0900

    MockMVC 테스트로 Controller 테스트

--- src/main/java/com/reheat/reheatlog/controller/PostController.java
+++ src/main/java/com/reheat/reheatlog/controller/PostController.java
@@ -1,21 +1,36 @@
 package com.reheat.reheatlog.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.reheat.reheatlog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
+@Slf4j
 public class PostController {
     //GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
-
     //글 등록은 POST
+//    @PostMapping("/posts")
+//    public String post(@RequestParam String title, @RequestParam String content) {
+//        log.info("title={}, content={}", title, content);
+//        return "post 성공!";
+//    }
+
+//    @PostMapping("/posts")
+//    public String post(@RequestParam Map<String, String> params) {
+//        return "post 성공!";
+//    }
+
     @PostMapping("/posts")
-    public String post(){
-        return "post 성공!";
+    public String post(@RequestBody PostCreate params) {
+        log.info("params={}", params.toString());
+        return "Hello World";
     }
 
+
     @GetMapping("/posts")
-    public String get(){
+    public String get() {
         return "Hello World";
     }
 }
--- src/test/java/com/reheat/reheatlog/controller/PostControllerTest.java
+++ src/test/java/com/reheat/reheatlog/controller/PostControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -19,8 +20,29 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 Hello World 출력")
     public void test() throws Exception {
+        //글 제목
+        //글 내용
+        //사용자
+            // id
+            // name
+            // level
+
+        /**
+         * {
+         *     "title" : "xxx",
+         *     "content" : "XXX",
+         *     "user"
+         *
+         * }
+         */
+
         //expected
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
+                        .param("title", "글 제목입니다")
+                        .param("content", "글 내용입니다")
+                ) //MockMVC content-Type은 application/json
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World"))
                 .andDo(print())
