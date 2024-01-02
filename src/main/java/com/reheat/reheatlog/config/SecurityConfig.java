2511bb85c9d4a3dfb7ab2393574291a25871f722
reheat <reheat1540@gmail.com>
2024-01-02 21:39:45 +0900

    feat: 시큐리티 폼 로그인 설정 및 시큐리티 JDBC 세션 추가

--- src/main/java/com/reheat/reheatlog/config/SecurityConfig.java
+++ src/main/java/com/reheat/reheatlog/config/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
@@ -28,7 +34,33 @@ public class SecurityConfig {
                 .requestMatchers("/auth/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .formLogin()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/auth/login")
+                .defaultSuccessUrl("/")
+                .and()
+                .userDetailsService(userDetailsService())
                 .csrf(csrf -> csrf.disable())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("reheat")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+        manager.createUser(user);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
