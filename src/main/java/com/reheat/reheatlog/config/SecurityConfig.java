8a2777f708ceeb15e30369b34f86b98af4000e75
reheat <reheat1540@gmail.com>
2024-01-04 10:04:23 +0900

    feat: 시큐리티 역할과 권한에 따른 페이지 접근 처리

--- src/main/java/com/reheat/reheatlog/config/SecurityConfig.java
+++ src/main/java/com/reheat/reheatlog/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.reheat.reheatlog.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reheat.reheatlog.config.handler.Http401Handler;
+import com.reheat.reheatlog.config.handler.Http403Handler;
+import com.reheat.reheatlog.config.handler.LoginFailHandler;
 import com.reheat.reheatlog.domain.User;
 import com.reheat.reheatlog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -16,9 +21,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final ObjectMapper objectMapper;
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -32,17 +41,24 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                .requestMatchers(HttpMethod.POST, "/auth/signup").permitAll()
+                .requestMatchers("/auth/login").permitAll()
+                .requestMatchers("/auth/signup").permitAll()
+                .requestMatchers("/user").hasRole("USER")
+                .requestMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/auth/login")
-                .loginProcessingUrl("/auth/login")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .defaultSuccessUrl("/")
+                    .loginPage("/auth/login")
+                    .loginProcessingUrl("/auth/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/")
+                    .failureHandler(new LoginFailHandler(objectMapper))
                 .and()
+                .exceptionHandling(e -> {
+                    e.accessDeniedHandler(new Http403Handler(objectMapper));
+                    e.authenticationEntryPoint(new Http401Handler(objectMapper));
+                })
                 .rememberMe(rm -> rm.rememberMeParameter("remember")
                         .alwaysRemember(false)
                         .tokenValiditySeconds(2592000)//30일
--- src/main/java/com/reheat/reheatlog/config/UserPrincipal.java
+++ src/main/java/com/reheat/reheatlog/config/UserPrincipal.java
@@ -8,11 +8,15 @@ import java.util.List;
 public class UserPrincipal extends User {
     private final Long userId;
 
+    // role : 역할 -> 관리자, 사용자, 매니저
+    // authority : 권한 -> 글쓰기, 글 읽기, 사용자 정지시키기
+
     public UserPrincipal(com.reheat.reheatlog.domain.User user) {
         super(user.getEmail(), user.getPassword(),
                 List.of(
-                        new SimpleGrantedAuthority("ROLE_ADMIN")
+                        new SimpleGrantedAuthority("ROLE_USER")
                 ));
+
         this.userId = user.getId();
     }
 
